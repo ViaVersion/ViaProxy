8228f035494789d70183eaa3397cfd696d341f94
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-02-01 17:42:43 +0100

    Fixed configs being reset to default on startup

--- src/main/java/net/raphimc/viaproxy/protocolhack/ProtocolHack.java
+++ src/main/java/net/raphimc/viaproxy/protocolhack/ProtocolHack.java
@@ -18,18 +18,49 @@
 package net.raphimc.viaproxy.protocolhack;
 
 import net.raphimc.viaprotocolhack.ViaProtocolHack;
+import net.raphimc.viaprotocolhack.impl.platform.ViaBackwardsPlatformImpl;
 import net.raphimc.viaprotocolhack.impl.platform.ViaLegacyPlatformImpl;
+import net.raphimc.viaprotocolhack.impl.platform.ViaRewindPlatformImpl;
 import net.raphimc.viaproxy.plugins.PluginManager;
 import net.raphimc.viaproxy.plugins.events.ProtocolHackInitEvent;
-import net.raphimc.viaproxy.protocolhack.impl.*;
+import net.raphimc.viaproxy.protocolhack.impl.ViaAprilFoolsPlatformImpl;
+import net.raphimc.viaproxy.protocolhack.impl.ViaProxyVPLoader;
+import net.raphimc.viaproxy.protocolhack.impl.ViaProxyViaVersionPlatformImpl;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class ProtocolHack {
 
     public static void init() {
+        patchConfigs();
         final Supplier<?>[] additionalPlatformSuppliers = PluginManager.EVENT_MANAGER.call(new ProtocolHackInitEvent(ViaAprilFoolsPlatformImpl::new)).getPlatformSuppliers().toArray(new Supplier[0]);
-        ViaProtocolHack.init(new ViaProxyViaVersionPlatformImpl(), new ViaProxyVPLoader(), null, null, ViaProxyViaBackwardsPlatformImpl::new, ViaProxyViaRewindPlatformImpl::new, ViaLegacyPlatformImpl::new, additionalPlatformSuppliers);
+        ViaProtocolHack.init(new ViaProxyViaVersionPlatformImpl(), new ViaProxyVPLoader(), null, null, ViaBackwardsPlatformImpl::new, ViaRewindPlatformImpl::new, ViaLegacyPlatformImpl::new, additionalPlatformSuppliers);
+    }
+
+    private static void patchConfigs() {
+        final File configFolder = new File("ViaProtocolHack");
+
+        final File viaVersionConfig = new File(configFolder, "viaversion.yml");
+        final Map<String, Object> viaVersionPatches = new HashMap<>();
+        viaVersionPatches.put("1_13-tab-complete-delay", 5);
+        viaVersionPatches.put("no-delay-shield-blocking", true);
+        viaVersionPatches.put("chunk-border-fix", true);
+        new ConfigPatcher(viaVersionConfig, viaVersionPatches);
+
+        final File viaBackwardsConfig = new File(configFolder, "config.yml");
+        final Map<String, Object> viaBackwardsPatches = new HashMap<>();
+        viaBackwardsPatches.put("fix-1_13-face-player", true);
+        viaBackwardsPatches.put("handle-pings-as-inv-acknowledgements", true);
+        new ConfigPatcher(viaBackwardsConfig, viaBackwardsPatches);
+
+        final File viaRewindConfig = new File(configFolder, "viarewind.yml");
+        final Map<String, Object> viaRewindPatches = new HashMap<>();
+        viaRewindPatches.put("replace-adventure", true);
+        viaRewindPatches.put("replace-particles", true);
+        new ConfigPatcher(viaRewindConfig, viaRewindPatches);
     }
 
 }
--- src/main/java/net/raphimc/viaproxy/protocolhack/impl/ViaProxyViaVersionPlatformImpl.java
+++ src/main/java/net/raphimc/viaproxy/protocolhack/impl/ViaProxyViaVersionPlatformImpl.java
@@ -17,13 +17,9 @@
  */
 package net.raphimc.viaproxy.protocolhack.impl;
 
-import com.viaversion.viaversion.configuration.AbstractViaConfig;
 import net.raphimc.viaprotocolhack.impl.platform.ViaVersionPlatformImpl;
-import net.raphimc.viaprotocolhack.impl.viaversion.VPViaConfig;
 import net.raphimc.viaproxy.cli.ConsoleFormatter;
 
-import java.io.File;
-import java.net.URL;
 import java.util.UUID;
 
 public class ViaProxyViaVersionPlatformImpl extends ViaVersionPlatformImpl {
@@ -35,18 +31,6 @@ public class ViaProxyViaVersionPlatformImpl extends ViaVersionPlatformImpl {
     @Override
     public void sendMessage(UUID uuid, String msg) {
         super.sendMessage(uuid, ConsoleFormatter.convert(msg));
-    }
-
-    @Override
-    protected AbstractViaConfig createConfig() {
-        new VPViaConfig(new File(this.getDataFolder(), "viaversion.yml")) {
-            @Override
-            public URL getDefaultConfigURL() {
-                return ViaProxyViaVersionPlatformImpl.class.getClassLoader().getResource("assets/viaproxy/config_diff/viaversion.yml");
-            }
-        }.reloadConfig();
-
-        return super.createConfig();
     }
 
 }
