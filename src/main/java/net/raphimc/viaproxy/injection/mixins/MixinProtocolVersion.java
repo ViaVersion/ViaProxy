3edef445f1004e1c4f688655335e7980ff86e219
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-03-09 18:25:32 +0100

    Updated Via*

--- src/main/java/net/raphimc/viaproxy/injection/mixins/MixinProtocolVersion.java
+++ src/main/java/net/raphimc/viaproxy/injection/mixins/MixinProtocolVersion.java
@@ -45,13 +45,12 @@ public abstract class MixinProtocolVersion {
     private static void initMaps(CallbackInfo ci) {
         skips = ImmutableSet.of("1.4.6/7", "1.5.1", "1.5.2", "1.6.1", "1.6.2", "1.6.3", "1.6.4");
         remaps = new HashMap<>();
-        remaps.put("1.7-1.7.5", new Pair<>("1.7.2-1.7.5", new VersionRange("1.7", 2, 5)));
         remaps.put("1.9.3/4", new Pair<>("1.9.3-1.9.4", null));
         remaps.put("1.11.1/2", new Pair<>("1.11.1-1.11.2", null));
         remaps.put("1.16.4/5", new Pair<>("1.16.4-1.16.5", null));
         remaps.put("1.18/1.18.1", new Pair<>("1.18-1.18.1", null));
         remaps.put("1.19.1/2", new Pair<>("1.19.1-1.19.2", null));
-        remaps.put("1.19.4", new Pair<>("1.19.4-pre3", null));
+        remaps.put("1.19.4", new Pair<>("1.19.4-rc1", null));
     }
 
     @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/viaversion/viaversion/api/protocol/version/ProtocolVersion;register(ILjava/lang/String;)Lcom/viaversion/viaversion/api/protocol/version/ProtocolVersion;"))
