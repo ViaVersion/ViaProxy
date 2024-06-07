9ecb61086aee06bd1b5b9b7e01c5059e3228c74d
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-06-07 20:05:54 +0200

    Use expiring map for temporary transfer data storage

--- src/main/java/net/raphimc/viaproxy/proxy/util/TransferDataHolder.java
+++ src/main/java/net/raphimc/viaproxy/proxy/util/TransferDataHolder.java
@@ -17,18 +17,19 @@
  */
 package net.raphimc.viaproxy.proxy.util;
 
+import com.google.common.cache.CacheBuilder;
 import com.viaversion.viabackwards.protocol.v1_20_5to1_20_3.storage.CookieStorage;
 import io.netty.channel.Channel;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class TransferDataHolder {
 
-    private static final Map<InetAddress, InetSocketAddress> TEMP_REDIRECTS = new ConcurrentHashMap<>();
-    private static final Map<InetAddress, CookieStorage> COOKIE_STORAGES = new ConcurrentHashMap<>();
+    private static final Map<InetAddress, InetSocketAddress> TEMP_REDIRECTS = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).<InetAddress, InetSocketAddress>build().asMap();
+    private static final Map<InetAddress, CookieStorage> COOKIE_STORAGES = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).<InetAddress, CookieStorage>build().asMap();
 
     public static void addTempRedirect(final Channel channel, final InetSocketAddress redirect) {
         TEMP_REDIRECTS.put(getChannelAddress(channel), redirect);
