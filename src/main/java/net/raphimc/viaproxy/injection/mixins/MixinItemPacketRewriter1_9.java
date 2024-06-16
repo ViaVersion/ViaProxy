f439382ee54454938ec07f1f93d68bc26ba3b03d
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-06-16 18:27:05 +0200

    Added 1.21 client and server support

--- src/main/java/net/raphimc/viaproxy/injection/mixins/MixinItemPacketRewriter1_9.java
+++ src/main/java/net/raphimc/viaproxy/injection/mixins/MixinItemPacketRewriter1_9.java
@@ -21,7 +21,7 @@ package net.raphimc.viaproxy.injection.mixins;
 import com.viaversion.nbt.tag.CompoundTag;
 import com.viaversion.nbt.tag.ListTag;
 import com.viaversion.viaversion.api.minecraft.item.Item;
-import com.viaversion.viaversion.api.minecraft.item.data.ModifierData;
+import com.viaversion.viaversion.api.minecraft.item.data.AttributeModifiers1_20_5;
 import com.viaversion.viaversion.libs.fastutil.ints.Int2ObjectMap;
 import com.viaversion.viaversion.libs.fastutil.ints.Int2ObjectOpenHashMap;
 import com.viaversion.viaversion.libs.gson.JsonElement;
@@ -51,7 +51,7 @@ public abstract class MixinItemPacketRewriter1_9 extends ItemRewriter<Clientboun
     private final Int2ObjectMap<String> itemIdentifiers = new Int2ObjectOpenHashMap<>();
 
     @Unique
-    private final Map<String, Map<String, Pair<String, ModifierData>>> itemAttributes = new HashMap<>();
+    private final Map<String, Map<String, Pair<String, AttributeModifiers1_20_5.ModifierData>>> itemAttributes = new HashMap<>();
 
     // TODO: ClassTransform bug: Constructor merging is broken
     /*public MixinItemPacketRewriter1_9(Protocol1_8To1_9 protocol, Type<Item> itemType, Type<Item[]> itemArrayType, Type<Item> mappedItemType, Type<Item[]> mappedItemArrayType) {
@@ -72,11 +72,11 @@ public abstract class MixinItemPacketRewriter1_9 extends ItemRewriter<Clientboun
         final JsonObject itemAttributes = ViaProxyMappingDataLoader.INSTANCE.loadData("item-attributes-1.8.json");
         for (Map.Entry<String, JsonElement> itemEntry : itemAttributes.entrySet()) {
             final String itemIdentifier = itemEntry.getKey();
-            final Map<String, Pair<String, ModifierData>> attributes = new HashMap<>();
+            final Map<String, Pair<String, AttributeModifiers1_20_5.ModifierData>> attributes = new HashMap<>();
             for (Map.Entry<String, JsonElement> attributeEntry : itemEntry.getValue().getAsJsonObject().entrySet()) {
                 final String attribute = attributeEntry.getKey();
                 final JsonObject attributeData = attributeEntry.getValue().getAsJsonObject();
-                final ModifierData modifierData = new ModifierData(UUID.fromString(attributeData.get("id").getAsString()), attributeData.get("name").getAsString(), attributeData.get("amount").getAsDouble(), attributeData.get("operation").getAsInt());
+                final AttributeModifiers1_20_5.ModifierData modifierData = new AttributeModifiers1_20_5.ModifierData(UUID.fromString(attributeData.get("id").getAsString()), attributeData.get("name").getAsString(), attributeData.get("amount").getAsDouble(), attributeData.get("operation").getAsInt());
                 final String slot = attributeData.get("slot").getAsString();
                 attributes.put(attribute, new Pair<>(slot, modifierData));
             }
@@ -91,7 +91,7 @@ public abstract class MixinItemPacketRewriter1_9 extends ItemRewriter<Clientboun
 
         final String identifier = this.itemIdentifiers.get(item.identifier());
         if (identifier != null && this.itemAttributes.containsKey(identifier)) {
-            final Map<String, Pair<String, ModifierData>> attributes = this.itemAttributes.get(identifier);
+            final Map<String, Pair<String, AttributeModifiers1_20_5.ModifierData>> attributes = this.itemAttributes.get(identifier);
             final CompoundTag attributeFixTag = new CompoundTag();
             CompoundTag tag = item.tag();
             if (tag == null) {
@@ -104,7 +104,7 @@ public abstract class MixinItemPacketRewriter1_9 extends ItemRewriter<Clientboun
             ListTag<CompoundTag> attributeModifiers = tag.getListTag("AttributeModifiers", CompoundTag.class);
             if (attributeModifiers == null) {
                 attributeModifiers = new ListTag<>(CompoundTag.class);
-                for (Map.Entry<String, Pair<String, ModifierData>> entry : attributes.entrySet()) {
+                for (Map.Entry<String, Pair<String, AttributeModifiers1_20_5.ModifierData>> entry : attributes.entrySet()) {
                     final CompoundTag attributeModifier = new CompoundTag();
                     attributeModifier.putString("AttributeName", entry.getKey());
                     attributeModifier.putString("Name", entry.getValue().value().name());
