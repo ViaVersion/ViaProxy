87de244ab4fe02f4aac6c79b5c077559da440674
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-08-23 15:12:23 +0200

    Updated Via API usage

--- src/main/java/net/raphimc/viaproxy/protocoltranslator/viaproxy/loading_terrain_fix/SpawnPositionTracker.java
+++ src/main/java/net/raphimc/viaproxy/protocoltranslator/viaproxy/loading_terrain_fix/SpawnPositionTracker.java
@@ -40,7 +40,7 @@ public class SpawnPositionTracker extends StoredObject {
     }
 
     public void sendSpawnPosition() {
-        final PacketWrapper setDefaultSpawnPosition = PacketWrapper.create(ClientboundPackets1_18.SET_DEFAULT_SPAWN_POSITION, this.getUser());
+        final PacketWrapper setDefaultSpawnPosition = PacketWrapper.create(ClientboundPackets1_18.SET_DEFAULT_SPAWN_POSITION, this.user());
         setDefaultSpawnPosition.write(Types.BLOCK_POSITION1_14, this.spawnPosition); // position
         setDefaultSpawnPosition.write(Types.FLOAT, this.angle); // angle
         setDefaultSpawnPosition.send(Protocol1_18To1_18_2.class);
