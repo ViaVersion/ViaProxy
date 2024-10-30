1287cb2f9f004c93349a36ec37fd38e561fc89cb
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-10-30 22:27:31 +0100

    Don't allow blank server address

--- src/main/java/net/raphimc/viaproxy/util/AddressUtil.java
+++ src/main/java/net/raphimc/viaproxy/util/AddressUtil.java
@@ -31,6 +31,10 @@ import java.net.SocketAddress;
 public class AddressUtil {
 
     public static SocketAddress parse(final String serverAddress, final ProtocolVersion version) {
+        if (serverAddress.isBlank()) {
+            throw new IllegalArgumentException("Server address cannot be blank");
+        }
+
         if (serverAddress.startsWith("file:///") || serverAddress.startsWith("unix:///")) { // Unix Socket
             final String filePath = serverAddress.substring(7);
 
